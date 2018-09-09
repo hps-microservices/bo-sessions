17c9fd9ea2d20e7a53dc7e890bea9ec433474a91
Felipe Santaniello <felipe_sbc_sp@hotmail.com>
2018-09-08 22:48:59 -0300

    first Commit

---
+++ src/main/java/com/msvvdv/bosessions/BoSessionsApplication.java
@@ -0,0 +1,12 @@
+package com.msvvdv.bosessions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoSessionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoSessionsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/msvvdv/bosessions/BoSessionsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.msvvdv.bosessions;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoSessionsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
